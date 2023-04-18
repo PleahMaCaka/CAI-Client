5ac017655757a0f4b660216d2cbaa5b6f88fb2de
pleahmacaka <pleahmacaka@gmail.com>
2023-04-18 22:06:22 +0900

    style: reformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 import MainFrame from './components/frames/main-frame'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
     <HashRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element={<MainFrame />}/>
+          <Route path='/' element={<MainFrame/>}/>
         </Routes>
       </div>
     </HashRouter>
