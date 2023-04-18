fcacf899e4309abcd88d24e5d062e5e76d8124a4
PleahMaCaka <PleahMaCaka@gmail.com>
2023-04-18 15:04:20 +0900

    chore: fix frame and margin stuff

--- src/components/chars-frame.tsx
+++ src/components/chars-frame.tsx
@@ -1,11 +1,24 @@
 import "../styles/chars-frame.css"
+import HomeSVG from "../assets/home.svg"
+import { useState } from "react"
 
 export default function CharsFrame() {
+  
+  const [show, setShow] = useState(false)
+
+  const addCharacter = () => {
+
+  }
+
   return (
     <>
       <div id="chars-frame">
-        Chars
-        <button className="center-icon add-btn">
+        <button id="home" className="center">
+          <img src={HomeSVG} alt="home">
+          </img>
+        </button>
+        <hr id="contour"/>
+        <button id="add-btn" className="center">
           +
         </button>
       </div>
